5691abc501ea10b5d7573aaea8262af0c53634db
Max Korenkov <mkorenkov@gmail.com>
2007-12-03 15:16:44 +0000

    masterpages databind removed.
    git-svn-id: http://dev.ezhecom.com/svn/pbc@18 179d6c2c-9243-4f44-b038-fef1748950ba

--- web/MasterPages/Default.Master.cs
+++ web/MasterPages/Default.Master.cs
@@ -1,27 +1,8 @@
-using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 
 namespace AIM.PBC.Web.MasterPages
 {
-	public partial class Default : System.Web.UI.MasterPage
+	public partial class Default : MasterPage
 	{
-		protected override void OnInit(EventArgs e)
-		{
-			base.OnInit(e);
-			Load += new EventHandler(Page_Load);
-		}
-
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			DataBind();
-		}
 	}
 }
--- web/Private/MasterPages/Private.Master.cs
+++ web/Private/MasterPages/Private.Master.cs
@@ -1,27 +1,8 @@
-using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 
 namespace AIM.PBC.Web.Private.MasterPages
 {
-	public partial class Private : System.Web.UI.MasterPage
+	public partial class Private : MasterPage
 	{
-		protected override void OnInit(EventArgs e)
-		{
-			base.OnInit(e);
-			Load += new EventHandler(Page_Load);
-		}
-
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			DataBind();
-		}
 	}
 }
