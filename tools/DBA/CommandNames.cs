dc659d2464f40c3e676f2ef138fe2bbead5eaedd
Max Korenkov <mkorenkov@gmail.com>
2007-12-05 16:20:46 +0000

    dba replaced with sqlse.
    git-svn-id: http://dev.ezhecom.com/svn/pbc@31 179d6c2c-9243-4f44-b038-fef1748950ba

--- tools/DBA/CommandNames.cs
+++
@@ -1,9 +0,0 @@
-namespace AIM.Tools.SqlSE
-{
-	public class CommandNames
-	{
-		public const string Execute = "-e";
-		public const string SearchAndExecute = "-se";
-		public const string ShowHelp = "-?";
-	}
-}
